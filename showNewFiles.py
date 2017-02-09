e81693a740dd69dd9243956a78c17d4db5fd171d
Reza <arbasli2@users.noreply.github.com>
2017-02-09 09:18:54 +0100

    usage string

--- showNewFiles.py
+++ showNewFiles.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 '''
 1- usage in Zim custum tools:
+    download the showNewFiles.py file. Under linux don't forget to make it executable, chmod +x showNewFiles.py. 
+    
     add the following line to a new costum tool:
-        ~/showNewFiles.py %n 10 
+    
+    * under linux: 
+        ~/showNewFiles.py %n 10
+    * under windows: if it is in a path for example "c:" 
+        python c:\showNewFiles.py %n 10
+
     check the box which says: Output should replace the current selection
-    in any page that you want to see the output select a temporary text, then run the tool and it will replace
+    in any page that you want to see the output select a **temporary text**, then run the tool and it will replace
     the selected text with the links to the last n (here: 10) modified pages. To make the links effective, press 
     CTRL+R to refresh the page.
     
